8802994ebe71d1495375b04f99c49c2fc4e59b2d
Slady32 <christoph.sladkovsky@gmail.com>
2014-03-02 11:13:36 +0100

    create unit tests

---
+++ ConvexHull/ConvexHull.Tests/GraphFactoryTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace ConvexHull.Tests
+{
+    [TestClass]
+    public class GraphFactoryTests
+    {
+        GraphFactory factory;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            factory = new GraphFactory();
+        }
+
+        [TestMethod]
+        public void TestCreateGraphWithList_EmptyList()
+        {
+            var points = new List<Point>();
+
+            var graph = factory.GenerateGraphWithList(new Point(0, 0), points);
+
+            Assert.AreEqual(graph.Nodes.Count, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void TestCreateGraphWithList_NullList()
+        {
+            var graph = factory.GenerateGraphWithList(new Point(0, 0), null);
+        }
+    }
+}
