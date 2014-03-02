185b0240f82506fc61e4016120ab1467d74c825b
Slady32 <christoph.sladkovsky@gmail.com>
2014-03-02 15:00:45 +0100

    extension method for IList

--- ConvexHull/ConvexHull/Graph.cs
+++ ConvexHull/ConvexHull/Graph.cs
@@ -13,25 +13,30 @@ namespace ConvexHull
         public Guid Id { get; set; }
         public Point Origin { get; private set; }
         public IList<Node> Nodes { get; set; }
+        public IList<Node> HullNodes { get; set; }
 
         public Graph(Point origin)
         {
             Id = Guid.NewGuid();
             Origin = origin;
             Nodes = new List<Node>();
+            HullNodes = new List<Node>();
         }
 
         public void Paint(PaintEventArgs e)
         {
-            foreach (var node in Nodes)
-            {
-                e.Graphics.DrawRectangle(Pens.Black, Origin.X + node.Position.X, Origin.Y + node.Position.Y, 5f, 5f);
+            Nodes.ForEach(n => PaintNode(n, e, Pens.Black));
+            HullNodes.ForEach(n => PaintNode(n, e, Pens.Blue));
+        }
 
-                var startPoint = new Point(node.Position.X + Origin.X,node.Position.Y + Origin.Y);
-                var endPoint = new Point(node.Next.Position.X + Origin.X, node.Next.Position.Y + Origin.Y);
+        private void PaintNode(Node node, PaintEventArgs e, Pen pen)
+        {
+            e.Graphics.DrawRectangle(pen, Origin.X + node.Position.X, Origin.Y + node.Position.Y, 5f, 5f);
 
-                e.Graphics.DrawLine(Pens.Black, startPoint, endPoint);
-            }
+            var startPoint = new Point(node.Position.X + Origin.X, node.Position.Y + Origin.Y);
+            var endPoint = new Point(node.Next.Position.X + Origin.X, node.Next.Position.Y + Origin.Y);
+
+            e.Graphics.DrawLine(pen, startPoint, endPoint);
         }
     }
 }
