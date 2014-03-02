140ed70d87455ec983c39688e5918f69301cb1ef
fatestitcher <sebastian.rodler@gmail.com>
2014-03-02 10:08:43 +0100

    Added Points and Nodes

---
+++ ConvexHull/ConvexHull/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvexHull
+{
+    public class Node
+    {
+        public readonly Guid Id { get; private set; }
+        public Point Position { get; private set; }
+        public Node Next { get; set; }
+        public Node Prev { get; set; }
+
+        public Node(Point position)
+        {
+            Position = position;
+            Id = Guid.NewGuid();
+        }
+    }
+
+}
