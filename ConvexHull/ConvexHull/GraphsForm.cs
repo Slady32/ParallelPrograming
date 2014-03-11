99808d8127a49b894bd428cb356753982073c87e
fatestitcher <sebastian.rodler@gmail.com>
2014-03-11 20:28:53 +0100

    Testing

--- ConvexHull/ConvexHull/GraphsForm.cs
+++ ConvexHull/ConvexHull/GraphsForm.cs
@@ -20,23 +20,43 @@ namespace ConvexHull
         {
             InitializeComponent();
 
-            //var graphFactory = new GraphFactory();
-            //var pointList = new List<Point>
-            //{
-            //    new Point(5, 62),
-            //    new Point(50, 25),
-            //    new Point(99, 56),
-            //    new Point(50, 125),
-            //    new Point(100, 100),
-            //    new Point(15, 130),
-            //    new Point(80, 135),
-            //    new Point(50, 150),
-            //    new Point(0, 100),
-            //    new Point(50, 50)
-            //};
-            //var graph = graphFactory.GenerateGraphWithList(new Point(100, 100), pointList);
+            var graphFactory = new GraphFactory();
+            var pointList = new List<Point>
+            {
+                new Point(62, 45),
+                new Point(17, 112),
+                new Point(108, 113),
+                new Point(126, 135),
+                new Point(53, 129),
+                new Point(85, 44),
+                new Point(80, 115),
+                new Point(28, 4),
+                new Point(96, 90),
+                new Point(85, 163),
+                new Point(72, 78),
+                new Point(100, 29),
+                new Point(153, 96),
+                new Point(184, 164),
+                new Point(183, 79)
+            };
+            var graph = graphFactory.GenerateGraphWithList(new Point(100, 100), pointList);
+            Graphs = new List<IPainter>
+            {
+                graph
+            };
 
-            GenerateRandomGraph();
+            var sb = new StringBuilder();
+            foreach (var point in graph.Points)
+            {
+                sb.Append(string.Format("new Point({0}, {1}),{2}", point.X, point.Y, Environment.NewLine));
+            }
+
+            sb.Remove(sb.Length - 3, 3);
+            textBox1.Text = sb.ToString();
+
+            _hull = new QuickHull(graph);
+            _hull.Execute();
+            //GenerateRandomGraph();
         }
 
         protected override void OnPaint(PaintEventArgs e)
