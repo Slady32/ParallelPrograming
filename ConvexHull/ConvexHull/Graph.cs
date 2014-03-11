1be3402dfa785100f0f620ef5f22c69a25a6435a
fatestitcher <sebastian.rodler@gmail.com>
2014-03-11 18:59:42 +0100

    Nur schnell den Graphen aufgehübscht, damit debuggen leichter wird

--- ConvexHull/ConvexHull/Graph.cs
+++ ConvexHull/ConvexHull/Graph.cs
@@ -27,6 +27,41 @@ namespace ConvexHull
         {
             Points.ForEach(p => PaintPoints(p, e, Pens.Black));
             HullNodes.ForEach(n => PaintNode(n, e, Pens.Blue));
+            PaintBaseLine(HullNodes[0], HullNodes[1], e, Pens.Red);
+            PaintCoordinateSystem(e, Pens.Green);
+        }
+
+        private void PaintCoordinateSystem(PaintEventArgs e, Pen pen)
+        {
+            var range = 300;
+            // Lines
+            e.Graphics.DrawLine(pen, Origin, new Point(range + Origin.X, Origin.Y));
+            e.Graphics.DrawLine(pen, Origin, new Point(Origin.X, range + Origin.Y));
+
+            // Qualifiers
+            for (int i = 0; i <= range; i += 50)
+            {
+                var startPoint = new Point(Origin.X + i, Origin.Y);
+                var endPoint = new Point(Origin.X + i, Origin.Y - 10);
+                e.Graphics.DrawLine(pen, startPoint, endPoint);
+            }
+
+            for (int i = 0; i <= range; i += 50)
+            {
+                var startPoint = new Point(Origin.X, Origin.Y + i);
+                var endPoint = new Point(Origin.X - 10, Origin.Y + i);
+                e.Graphics.DrawLine(pen, startPoint, endPoint);
+            }
+
+
+        }
+
+        private void PaintBaseLine(Node node1, Node node2, PaintEventArgs e, Pen pen)
+        {
+            var startPoint = new Point(node1.Position.X + Origin.X, node1.Position.Y + Origin.Y);
+            var endPoint = new Point(node2.Position.X + Origin.X, node2.Position.Y + Origin.Y);
+
+            e.Graphics.DrawLine(pen, startPoint, endPoint);
         }
 
         private void PaintPoints(Point point, PaintEventArgs e, Pen pen)
