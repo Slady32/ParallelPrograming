d1bffd39eaf9e69ca177f4dbaf353b5291676a08
fatestitcher <sebastian.rodler@gmail.com>
2014-03-12 18:07:09 +0100

    Aufgehübscht^^

--- ConvexHull/ConvexHull/GraphsForm.cs
+++ ConvexHull/ConvexHull/GraphsForm.cs
@@ -16,37 +16,54 @@ namespace ConvexHull
 
         private IHull _hull;
 
+        private Graph _graph;
+
         public GraphsForm()
         {
             InitializeComponent();
 
-            var graphFactory = new GraphFactory();
-            var pointList = new List<Point>
+            InitializeGraph();
+
+        }
+
+        private void InitializeGraph()
+        {
+            var generateRandom = true;
+            if (!generateRandom)
             {
-                new Point(62, 45),
-                new Point(17, 112),
-                new Point(108, 113),
-                new Point(126, 135),
-                new Point(53, 129),
-                new Point(85, 44),
-                new Point(80, 115),
-                new Point(28, 4),
-                new Point(96, 90),
-                new Point(85, 163),
-                new Point(72, 78),
-                new Point(100, 29),
-                new Point(153, 96),
-                new Point(184, 164),
-                new Point(183, 79)
-            };
-            var graph = graphFactory.GenerateGraphWithList(new Point(100, 100), pointList);
+                var graphFactory = new GraphFactory();
+                var pointList = new List<Point>
+                {
+                    new Point(62, 45),
+                    new Point(17, 112),
+                    new Point(108, 113),
+                    new Point(126, 135),
+                    new Point(53, 129),
+                    new Point(85, 44),
+                    new Point(80, 115),
+                    new Point(28, 4),
+                    new Point(96, 90),
+                    new Point(85, 163),
+                    new Point(72, 78),
+                    new Point(100, 29),
+                    new Point(153, 96),
+                    new Point(184, 164),
+                    new Point(183, 79)
+                };
+                    _graph = graphFactory.GenerateGraphWithList(new Point(100, 100), pointList);
+                    
+            }
+            else if (generateRandom)
+            {
+                _graph = new GraphFactory().GenerateGraphWithRandoms(new Point(100, 100), 25);
+            }
             Graphs = new List<IPainter>
-            {
-                graph
-            };
+                    {
+                        _graph
+                    };
 
             var sb = new StringBuilder();
-            foreach (var point in graph.Points)
+            foreach (var point in _graph.Points)
             {
                 sb.Append(string.Format("new Point({0}, {1}),{2}", point.X, point.Y, Environment.NewLine));
             }
@@ -54,9 +71,8 @@ namespace ConvexHull
             sb.Remove(sb.Length - 3, 3);
             textBox1.Text = sb.ToString();
 
-            _hull = new QuickHull(graph);
+            _hull = new QuickHull(_graph);
             _hull.Execute();
-            //GenerateRandomGraph();
         }
 
         protected override void OnPaint(PaintEventArgs e)
@@ -75,30 +91,8 @@ namespace ConvexHull
 
         private void GenGraph_Click(object sender, EventArgs e)
         {
-            GenerateRandomGraph();
+            InitializeGraph();
             Invalidate();
-        }
-
-        private void GenerateRandomGraph()
-        {
-            var graph = new GraphFactory().GenerateGraphWithRandoms(new Point(100, 100), 50);
-
-            Graphs = new List<IPainter>
-            {
-                graph
-            };
-
-            var sb = new StringBuilder();
-            foreach (var point in graph.Points)
-            {
-                sb.Append(string.Format("new Point({0}, {1}),{2}", point.X, point.Y, Environment.NewLine));
-            }
-
-            sb.Remove(sb.Length - 3, 3);
-            textBox1.Text = sb.ToString();
-
-            _hull = new QuickHull(graph);
-            _hull.Execute();
         }
     }
 }
